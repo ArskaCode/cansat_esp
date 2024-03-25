90f8c3cedf5ddb4e47ddfc8c0ffe78bd498317df
Joel Ventola <joe.te.vento2@gmail.com>
2024-03-25 14:52:22 +0200

    added log prints to steps of functions

--- main/ntc.c
+++ main/ntc.c
@@ -20,6 +20,7 @@ static int voltage[2][10];
 
 // Reads the current voltage from the ntc pin
 int ntc_read(){
+    ESP_LOGI(TAG, "Reading ntc voltage");
     ESP_ERROR_CHECK(adc_oneshot_read(adc2_handle, NTC_ADC2_CHAN, &adc_raw[1][0]));
     ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc2_cali_handle, adc_raw[1][0], &voltage[1][0]));
     ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_2 + 1, NTC_ADC2_CHAN, voltage[1][0]);
@@ -29,12 +30,14 @@ int ntc_read(){
 // Initialises the ADC channels for the ntc resistor
 void ntc_init(){
     //-------------ADC2 config---------------//
+    ESP_LOGI(TAG, "Configuring adc2");
     adc_oneshot_chan_cfg_t config = {
         .bitwidth = ADC_BITWIDTH_DEFAULT,
         .atten = NTC_ADC_ATTEN,
     };
 
     //-------------ADC2 Init---------------//
+    ESP_LOGI(TAG, "Initilazing adc2");
     adc_oneshot_unit_init_cfg_t init_config2 = {
         .unit_id = ADC_UNIT_2,
         .ulp_mode = ADC_ULP_MODE_DISABLE,
@@ -42,12 +45,15 @@ void ntc_init(){
     ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config2, &adc2_handle));
 
     //-------------ADC2 Calibration Init---------------//
+    ESP_LOGI(TAG, "Initializing adc2 calibration");
     bool do_calibration2 = NTC_adc_calibration_init(ADC_UNIT_2, NTC_ADC2_CHAN, NTC_ADC_ATTEN, &adc2_cali_handle);
 
     //-------------ADC2 Config---------------//
+    ESP_LOGI(TAG, "Adc2 config");
     ESP_ERROR_CHECK(adc_oneshot_config_channel(adc2_handle, NTC_ADC2_CHAN, &config));
     
     //-------------ADC2 Calibratin-------------//
+    ESP_LOGI(TAG, "Calibrating adc2");
     if (do_calibration2) {
         ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc2_cali_handle, adc_raw[1][0], &voltage[1][0]));
         ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_2 + 1, NTC_ADC2_CHAN, voltage[1][0]); 
