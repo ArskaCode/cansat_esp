af250a237cae691d2fe40cc7292dabafcf07699d
Joel Ventola <joe.te.vento2@gmail.com>
2024-03-25 14:45:29 +0200

    a bit of clean up

--- main/ntc.c
+++ main/ntc.c
@@ -17,8 +17,6 @@ adc_oneshot_unit_handle_t adc2_handle;
 
 static int adc_raw[2][10];
 static int voltage[2][10];
-static bool NTC_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
-//static void example_adc_calibration_deinit(adc_cali_handle_t handle); //prollly not needed
 
 // Reads the current voltage from the ntc pin
 int ntc_read(){
