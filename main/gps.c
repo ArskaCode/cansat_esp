0553c8313de38d0b26d4ccb3770b4711fac0252f
ArskaCode <39701454+ArskaCode@users.noreply.github.com>
2024-03-26 14:17:38 +0200

    Geps

--- main/gps.c
+++ main/gps.c
@@ -1,8 +1,19 @@
 #include "gps.h"
+#include "ext/nmea_parser.h"
 
-#include <string.h>
-#include "driver/uart.h"
-#include "driver/gpio.h"
-#include "driver/gptimer.h"
-#include "freertos/FreeRTOS.h"
-#include "esp_log.h"
+nmea_parser_handle_t nmea_hdl;
+
+void gps_init(void)
+{
+    nmea_parser_config_t config = NMEA_PARSER_CONFIG_DEFAULT();
+    nmea_hdl = nmea_parser_init(&config);
+}
+
+void gps_get_data(gps_data_t* gps_data)
+{
+    gps_t* gps = nmea_get_latest_gps(nmea_hdl);
+
+    gps_data->latitude = gps->latitude;
+    gps_data->longitude = gps->longitude;
+    gps_data->altitude = gps->altitude;
+}
--- main/lora.c
+++ main/lora.c
@@ -75,7 +75,6 @@ void lora_init(void)
     ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, CONFIG_LORA_TX, CONFIG_LORA_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
     ESP_ERROR_CHECK(uart_driver_install(UART_NUM_2, uart_buffer_size, uart_buffer_size, 10, NULL, 0));
 
-    #if CONFIG_LORA_M01_CONNECTED
     // M0 & M1 pin config
     gpio_config_t io_config_mode = {
         .intr_type = GPIO_INTR_DISABLE,
@@ -85,7 +84,6 @@ void lora_init(void)
         .pull_up_en = GPIO_PULLUP_DISABLE,
     };
     ESP_ERROR_CHECK(gpio_config(&io_config_mode));
-    #endif
 
     // aux pin config
     gpio_config_t io_config_aux = {
@@ -209,13 +207,8 @@ void lora_wait_aux(void)
 
 void lora_set_mode(lora_mode_t mode)
 {
-    #if CONFIG_LORA_M01_CONNECTED
     lora_state.mode = mode;
 
     ESP_ERROR_CHECK(gpio_set_level(CONFIG_LORA_M0, (mode >> 1) & 0b1));
     ESP_ERROR_CHECK(gpio_set_level(CONFIG_LORA_M1, mode & 0b1));
-    #else
-    assert(mode == LORA_MODE_TRANSMISSION);
-    lora_state.mode = LORA_MODE_TRANSMISSION;
-    #endif
 }
