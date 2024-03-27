a69156ec0f58b1c53c0d76d5bc54c91d32896d6a
Joel Ventola <joe.te.vento2@gmail.com>
2024-03-27 09:38:45 +0200

    Bit of cleanup

--- main/bmp280.c
+++ main/bmp280.c
@@ -1,11 +1,11 @@
 /**
- * BMX280 - BMP280 Driver for Esspressif ESP-32.
+ * BMP280 - BMP280 Driver for Esspressif ESP-32.
  *
  * MIT License
  *
  * Copyright (C) 2020 Halit Utku Maden
  * Please contact at <utkumaden@hotmail.com>
- * Mostly just stolen from https://github.com/utkumaden/esp-idf-bmx280
+ * Mostly just stolen from https://github.com/utkumaden/esp-idf-bmp280
  */
 #include "bmp280.h"
 #include "esp_log.h"
@@ -14,38 +14,41 @@
 #include <stdint.h>
 #include <string.h>
 
+
+static const char* TAG = "cansat_bmp280";
+
 // TODO none of these are setup correctly 
 
 // Register address of pressure fraction significant byte.
-#define BMX280_REG_PRES_XSB 0xF9
+#define BMP280_REG_PRES_XSB 0xF9
 // Register address of pressure least significant byte.
-#define BMX280_REG_PRES_LSB 0xF8
+#define BMP280_REG_PRES_LSB 0xF8
 // Register address of pressure most significant byte.
-#define BMX280_REG_PRES_MSB 0xF7
+#define BMP280_REG_PRES_MSB 0xF7
 
 // Register address of sensor configuration.
-#define BMX280_REG_CONFIG 0xF5
+#define BMP280_REG_CONFIG 0xF5
 // Register address of sensor measurement control.
-#define BMX280_REG_MESCTL 0xF4
+#define BMP280_REG_MESCTL 0xF4
 // Register address of sensor status.
-#define BMX280_REG_STATUS 0xF3
+#define BMP280_REG_STATUS 0xF3
 
 // Register address for sensor reset.
-#define BMX280_REG_RESET 0xE0
+#define BMP280_REG_RESET 0xE0
 // Chip reset vector.
-#define BMX280_RESET_VEC 0xB6
+#define BMP280_RESET_VEC 0xB6
 
 // [BME280] Register address of calibration constants. (high bank)
-#define BMX280_REG_CAL_HI 0xE1
+#define BMP280_REG_CAL_HI 0xE1
 // Register address of calibration constants. (low bank)
-#define BMX280_REG_CAL_LO 0x88
+#define BMP280_REG_CAL_LO 0x88
 
 // Register address for chip identification number.
-#define BMX280_REG_CHPID 0xD0
+#define BMP280_REG_CHPID 0xD0
 // Value of REG_CHPID for BMP280 (Production)
 #define BMP280_ID2 0x58
 
-struct bmx280_t{
+struct bmp280_t{
     // I2C port.
     i2c_port_t i2c_port;
     // Slave Address of sensor.
@@ -74,26 +77,26 @@ struct bmx280_t{
 /**
  * Macro to verify a the chip id matches with the expected value for BMP280.
  * @note Use when the chip needs to be verified as a BMP280.
- * @see bmx280_verify
+ * @see bmp280_verify
  * @param chip_id The chip id.
  */
-#define bmx280_verify(chip_id) ((chip_id) == BMP280_ID2)
+#define bmp280_verify(chip_id) ((chip_id) == BMP280_ID2)
 
 /**
  * Returns false if the sensor was not found.
- * @param bmx280 The driver structure.
+ * @param bmp280 The driver structure.
  */
-#define bmx280_validate(bmx280) (!(bmx280->slave == 0xDE && bmx280->chip_id == 0xAD))
+#define bmp280_validate(bmp280) (!(bmp280->slave == 0xDE && bmp280->chip_id == 0xAD))
 
 /**
  * Read from sensor.
- * @param bmx280 Driver Sturcture.
+ * @param bmp280 Driver Sturcture.
  * @param addr Register address.
  * @param dout Data to read.
  * @param size The number of bytes to read.
  * @returns Error codes.
  */
-static esp_err_t bmx280_read(bmx280_t *bmx280, uint8_t addr, uint8_t *dout, size_t size)
+static esp_err_t bmp280_read(bmp280_t *bmp280, uint8_t addr, uint8_t *dout, size_t size)
 {
     esp_err_t err;
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
@@ -101,17 +104,17 @@ static esp_err_t bmx280_read(bmx280_t *bmx280, uint8_t addr, uint8_t *dout, size
     {
         // Write register address
         i2c_master_start(cmd);
-        i2c_master_write_byte(cmd, bmx280->slave | I2C_MASTER_WRITE, true);
+        i2c_master_write_byte(cmd, bmp280->slave | I2C_MASTER_WRITE, true);
         i2c_master_write_byte(cmd, addr, true);
 
         // Read Registers
         i2c_master_start(cmd);
-        i2c_master_write_byte(cmd, bmx280->slave | I2C_MASTER_READ, true);
+        i2c_master_write_byte(cmd, bmp280->slave | I2C_MASTER_READ, true);
         i2c_master_read(cmd, dout, size, I2C_MASTER_LAST_NACK);
         i2c_master_stop(cmd);
 
         TickType_t ticks = pdMS_TO_TICKS(CONFIG_BMP280_TIMEOUT);
-        err = i2c_master_cmd_begin(bmx280->i2c_port, cmd, ticks);
+        err = i2c_master_cmd_begin(bmp280->i2c_port, cmd, ticks);
         i2c_cmd_link_delete(cmd);
         return err;
     }
@@ -123,13 +126,13 @@ static esp_err_t bmx280_read(bmx280_t *bmx280, uint8_t addr, uint8_t *dout, size
 /**
  * Write to sensor
  * 
- * @param bmx280 Driver Sturcture.
+ * @param bmp280 Driver Sturcture.
  * @param addr Register address.
  * @param din Data to write.
  * @param size The number of bytes to write.
  * @returns Error codes.
 */
-static esp_err_t bmx280_write(bmx280_t* bmx280, uint8_t addr, const uint8_t *din, size_t size)
+static esp_err_t bmp280_write(bmp280_t* bmp280, uint8_t addr, const uint8_t *din, size_t size)
 {
     esp_err_t err;
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
@@ -138,7 +141,7 @@ static esp_err_t bmx280_write(bmx280_t* bmx280, uint8_t addr, const uint8_t *din
         for (int i = 0; i < size; i++)
         {
             i2c_master_start(cmd);
-            i2c_master_write_byte(cmd, bmx280->slave | I2C_MASTER_WRITE, true);
+            i2c_master_write_byte(cmd, bmp280->slave | I2C_MASTER_WRITE, true);
             // Register
             i2c_master_write_byte(cmd, addr + i, true);
             //Data
@@ -148,7 +151,7 @@ static esp_err_t bmx280_write(bmx280_t* bmx280, uint8_t addr, const uint8_t *din
 
         
         TickType_t ticks = pdMS_TO_TICKS(CONFIG_BMP280_TIMEOUT);
-        err = i2c_master_cmd_begin(bmx280->i2c_port, cmd, ticks);
+        err = i2c_master_cmd_begin(bmp280->i2c_port, cmd, ticks);
         i2c_cmd_link_delete(cmd);
         return err;
     }
@@ -160,18 +163,18 @@ static esp_err_t bmx280_write(bmx280_t* bmx280, uint8_t addr, const uint8_t *din
 
 /**
  * Probes a given address
- * @param bmx280 Driver Sturcture.
+ * @param bmp280 Driver Sturcture.
  * @returns Error codes.
 */
-static esp_err_t bmx280_probe_address(bmx280_t *bmx280)
+static esp_err_t bmp280_probe_address(bmp280_t *bmp280)
 {
-    esp_err_t err = bmx280_read(bmx280, BMX280_REG_CHPID, &bmx280->chip_id, sizeof bmx280->chip_id);
+    esp_err_t err = bmp280_read(bmp280, BMP280_REG_CHPID, &bmp280->chip_id, sizeof bmp280->chip_id);
 
     if (err == ESP_OK)
     {
-        if (bmx280_verify(bmx280->chip_id))
+        if (bmp280_verify(bmp280->chip_id))
         {
-            ESP_LOGI("bmx280", "Probe success: address=%hhx, id=%hhx", bmx280->slave, bmx280->chip_id);
+            ESP_LOGI(TAG, "Probe success: address=%hhx, id=%hhx", bmp280->slave, bmp280->chip_id);
            return ESP_OK;
         }
         else
@@ -180,26 +183,26 @@ static esp_err_t bmx280_probe_address(bmx280_t *bmx280)
         }
     }
 
-    ESP_LOGW("bmx280", "Probe failure: address=%hhx, id=%hhx, reason=%s", bmx280->slave, bmx280->chip_id, esp_err_to_name(err));
+    ESP_LOGW("bmp280", "Probe failure: address=%hhx, id=%hhx, reason=%s", bmp280->slave, bmp280->chip_id, esp_err_to_name(err));
     return err;
 }
 /**
  * Probes the sensor
- * @param bmx280 Driver Sturcture.
+ * @param bmp280 Driver Sturcture.
  * @returns Error codes.
 */
-static esp_err_t bmx280_probe(bmx280_t *bmx280)
+static esp_err_t bmp280_probe(bmp280_t *bmp280)
 {
-    ESP_LOGI("bmx280", "Probing for BMP280 sensor on I2C %d", bmx280->i2c_port);
+    ESP_LOGI(TAG, "Probing for BMP280 sensor on I2C %d", bmp280->i2c_port);
 
     esp_err_t err;
-    bmx280->slave = 0xEC;
+    bmp280->slave = 0xEC;
 
-    if ((err = bmx280_probe_address(bmx280)) != ESP_OK)
+    if ((err = bmp280_probe_address(bmp280)) != ESP_OK)
     {
-        ESP_LOGE("bmx280", "Sensor not found.");
-        bmx280->slave = 0xDE;
-        bmx280->chip_id = 0xAD;
+        ESP_LOGE(TAG, "Sensor not found.");
+        bmp280->slave = 0xDE;
+        bmp280->chip_id = 0xAD;
     }
 
     return err;
@@ -207,45 +210,45 @@ static esp_err_t bmx280_probe(bmx280_t *bmx280)
 
 /**
  * Resets the sensor.
- * @param bmx280 Driver Sturcture.
+ * @param bmp280 Driver Sturcture.
  * @returns Error codes.
 */
-static esp_err_t bmx280_reset(bmx280_t *bmx280)
+static esp_err_t bmp280_reset(bmp280_t *bmp280)
 {
-    const static uint8_t din[] = { BMX280_RESET_VEC };
-    return bmx280_write(bmx280, BMX280_REG_RESET, din, sizeof din);
+    const static uint8_t din[] = { BMP280_RESET_VEC };
+    return bmp280_write(bmp280, BMP280_REG_RESET, din, sizeof din);
 }
 
 /**
  * Calibrates the sensor
- * @param bmx280 Driver Sturcture.
+ * @param bmp280 Driver Sturcture.
  * @returns Error codes.
 */
-static esp_err_t bmx280_calibrate(bmx280_t *bmx280)
+static esp_err_t bmp280_calibrate(bmp280_t *bmp280)
 {
-    ESP_LOGI("bmx280", "Reading out calibration values...");
+    ESP_LOGI(TAG, "Reading out calibration values...");
 
     esp_err_t err;
     uint8_t buf[26];
 
-    err = bmx280_read(bmx280, BMX280_REG_CAL_LO, buf, sizeof buf);
+    err = bmp280_read(bmp280, BMP280_REG_CAL_LO, buf, sizeof buf);
 
     if (err != ESP_OK) return err;
 
-    ESP_LOGI("bmx280", "Read Low Bank.");
+    ESP_LOGI(TAG, "Read Low Bank.");
 
-    bmx280->cmps.T1 = buf[0] | (buf[1] << 8);
-    bmx280->cmps.T2 = buf[2] | (buf[3] << 8);
-    bmx280->cmps.T3 = buf[4] | (buf[5] << 8);
-    bmx280->cmps.P1 = buf[6] | (buf[7] << 8);
-    bmx280->cmps.P2 = buf[8] | (buf[9] << 8);
-    bmx280->cmps.P3 = buf[10] | (buf[11] << 8);
-    bmx280->cmps.P4 = buf[12] | (buf[13] << 8);
-    bmx280->cmps.P5 = buf[14] | (buf[15] << 8);
-    bmx280->cmps.P6 = buf[16] | (buf[17] << 8);
-    bmx280->cmps.P7 = buf[18] | (buf[19] << 8);
-    bmx280->cmps.P8 = buf[20] | (buf[21] << 8);
-    bmx280->cmps.P9 = buf[22] | (buf[23] << 8);
+    bmp280->cmps.T1 = buf[0] | (buf[1] << 8);
+    bmp280->cmps.T2 = buf[2] | (buf[3] << 8);
+    bmp280->cmps.T3 = buf[4] | (buf[5] << 8);
+    bmp280->cmps.P1 = buf[6] | (buf[7] << 8);
+    bmp280->cmps.P2 = buf[8] | (buf[9] << 8);
+    bmp280->cmps.P3 = buf[10] | (buf[11] << 8);
+    bmp280->cmps.P4 = buf[12] | (buf[13] << 8);
+    bmp280->cmps.P5 = buf[14] | (buf[15] << 8);
+    bmp280->cmps.P6 = buf[16] | (buf[17] << 8);
+    bmp280->cmps.P7 = buf[18] | (buf[19] << 8);
+    bmp280->cmps.P8 = buf[20] | (buf[21] << 8);
+    bmp280->cmps.P9 = buf[22] | (buf[23] << 8);
 
     return ESP_OK;
 }
@@ -253,32 +256,32 @@ static esp_err_t bmx280_calibrate(bmx280_t *bmx280)
 /**
  * Creates the driver structure
  * @param i2c_port i2c port of the sensor.
- * @returns bmx280 Driver Sturcture.
+ * @returns bmp280 Driver Sturcture.
 */
-bmx280_t* bmx280_create(i2c_port_t port)
+bmp280_t* bmp280_create(i2c_port_t port)
 {
-    bmx280_t* bmx280 = malloc(sizeof(bmx280_t));
-    if (bmx280)
+    bmp280_t* bmp280 = malloc(sizeof(bmp280_t));
+    if (bmp280)
     {
-        memset(bmx280, 0, sizeof(bmx280_t));
+        memset(bmp280, 0, sizeof(bmp280_t));
 
-        bmx280->i2c_port = port;
-        bmx280->slave = 0xDE;
-        bmx280->chip_id = 0xAD;
+        bmp280->i2c_port = port;
+        bmp280->slave = 0xDE;
+        bmp280->chip_id = 0xAD;
     }
-    return bmx280;
+    return bmp280;
 }
 
 /**
  * Initializes the sensor
- * @param bmx280 Driver Sturcture.
+ * @param bmp280 Driver Sturcture.
  * @returns Error codes.
 */
-esp_err_t bmx280_init(bmx280_t* bmx280)
+esp_err_t bmp280_init(bmp280_t* bmp280)
 {
-    if (bmx280 == NULL) return ESP_ERR_INVALID_ARG;
+    if (bmp280 == NULL) return ESP_ERR_INVALID_ARG;
 
-    esp_err_t error = bmx280_probe(bmx280) || bmx280_reset(bmx280);
+    esp_err_t error = bmp280_probe(bmp280) || bmp280_reset(bmp280);
 
     if (error == ESP_OK)
     {
@@ -286,10 +289,10 @@ esp_err_t bmx280_init(bmx280_t* bmx280)
         vTaskDelay(pdMS_TO_TICKS(10));
 
         // Read calibration data.
-        bmx280_calibrate(bmx280);
+        bmp280_calibrate(bmp280);
 
-        ESP_LOGI("bmx280", "Dumping calibration...");
-        ESP_LOG_BUFFER_HEX("bmx280", &bmx280->cmps, sizeof(bmx280->cmps));
+        ESP_LOGI(TAG, "Dumping calibration...");
+        ESP_LOG_BUFFER_HEX("bmp280", &bmp280->cmps, sizeof(bmp280->cmps));
     }
 
     return error;
@@ -297,80 +300,80 @@ esp_err_t bmx280_init(bmx280_t* bmx280)
 
 /**
  * Configures the sensor
- * @param bmx280 Driver Sturcture.
+ * @param bmp280 Driver Sturcture.
  * @param cfg Driver Sturcture config.
  * @returns Error codes.
 */
-esp_err_t bmx280_configure(bmx280_t* bmx280, bmx280_config_t *cfg)
+esp_err_t bmp280_configure(bmp280_t* bmp280, bmp280_config_t *cfg)
 {
-    if (bmx280 == NULL || cfg == NULL) return ESP_ERR_INVALID_ARG;
-    if (!bmx280_validate(bmx280)) return ESP_ERR_INVALID_STATE;
+    if (bmp280 == NULL || cfg == NULL) return ESP_ERR_INVALID_ARG;
+    if (!bmp280_validate(bmp280)) return ESP_ERR_INVALID_STATE;
 
-    uint8_t num = (cfg->p_sampling << 2) | BMX280_MODE_SLEEP;
-    return bmx280_write(bmx280, BMX280_REG_MESCTL, &num, sizeof num);
+    uint8_t num = (cfg->p_sampling << 2) | BMP280_MODE_SLEEP;
+    return bmp280_write(bmp280, BMP280_REG_MESCTL, &num, sizeof num);
 }
 
 /**
  * Used to set the mode of the sensor. Not sure which number equals to what. 1 or 0 is prolly "Normal"
- * @param bmx280 Driver Sturcture.
+ * @param bmp280 Driver Sturcture.
  * @param mode Driver mode.
  * @returns Error codes.
 */
-esp_err_t bmx280_setMode(bmx280_t* bmx280, bmx280_mode_t mode)
+esp_err_t bmp280_setMode(bmp280_t* bmp280, bmp280_mode_t mode)
 {
     uint8_t ctrl_mes;
     esp_err_t err;
 
-    if ((err = bmx280_read(bmx280, BMX280_REG_MESCTL, &ctrl_mes, 1)) != ESP_OK)
+    if ((err = bmp280_read(bmp280, BMP280_REG_MESCTL, &ctrl_mes, 1)) != ESP_OK)
         return err;
 
     ctrl_mes = (ctrl_mes & (~3)) | mode;
 
-    return bmx280_write(bmx280, BMX280_REG_MESCTL, &ctrl_mes, 1);
+    return bmp280_write(bmp280, BMP280_REG_MESCTL, &ctrl_mes, 1);
 }
 
 // Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format (24 integer bits and 8 fractional bits).
 // Output value of “24674867” represents 24674867/256 = 96386.2 Pa = 963.862 hPa
-uint32_t BMP280_compensate_P_int64(bmx280_t *bmx280, int32_t adc_P)
+uint32_t BMP280_compensate_P_int64(bmp280_t *bmp280, int32_t adc_P)
 {
     int64_t var1, var2, p;
-    var1 = ((int64_t)bmx280->t_fine) -128000;
-    var2 = var1 * var1 * (int64_t)bmx280->cmps.P6;
-    var2 = var2 + ((var1*(int64_t)bmx280->cmps.P5)<<17);
-    var2 = var2 + (((int64_t)bmx280->cmps.P4)<<35);
-    var1 = ((var1 * var1 * (int64_t)bmx280->cmps.P3)>>8) + ((var1 * (int64_t)bmx280->cmps.P2)<<12);
-    var1 = (((((int64_t)1)<<47)+var1))*((int64_t)bmx280->cmps.P1)>>33;
+    var1 = ((int64_t)bmp280->t_fine) -128000;
+    var2 = var1 * var1 * (int64_t)bmp280->cmps.P6;
+    var2 = var2 + ((var1*(int64_t)bmp280->cmps.P5)<<17);
+    var2 = var2 + (((int64_t)bmp280->cmps.P4)<<35);
+    var1 = ((var1 * var1 * (int64_t)bmp280->cmps.P3)>>8) + ((var1 * (int64_t)bmp280->cmps.P2)<<12);
+    var1 = (((((int64_t)1)<<47)+var1))*((int64_t)bmp280->cmps.P1)>>33;
     if(var1 == 0){ 
         return 0; // avoid exception caused by division by zero
     }
     p = 1048576-adc_P;
     p = (((p<<31)-var2)*3125)/var1;
-    var1 = (((int64_t)bmx280->cmps.P9) * (p>>13) * (p>>13)) >> 25;
-    var2 =(((int64_t)bmx280->cmps.P8) * p) >> 19;
-    p = ((p + var1 + var2) >> 8) + (((int64_t)bmx280->cmps.P7)<<4);
+    var1 = (((int64_t)bmp280->cmps.P9) * (p>>13) * (p>>13)) >> 25;
+    var2 =(((int64_t)bmp280->cmps.P8) * p) >> 19;
+    p = ((p + var1 + var2) >> 8) + (((int64_t)bmp280->cmps.P7)<<4);
     return (uint32_t)p;
 }
 
 /**
  * Used to actually read the current pressure
- * @param bmx280 Driver Sturcture.
+ * @param bmp280 Driver Sturcture.
  * @returns Error codes.
 */
-esp_err_t bmx280_readout(bmx280_t *bmx280, uint32_t *pressure)
+esp_err_t bmp280_readout(bmp280_t *bmp280, uint32_t *pressure)
 {
-    if (bmx280 == NULL) return ESP_ERR_INVALID_ARG;
-    if (!bmx280_validate(bmx280)) return ESP_ERR_INVALID_STATE;
+    if (bmp280 == NULL) return ESP_ERR_INVALID_ARG;
+    if (!bmp280_validate(bmp280)) return ESP_ERR_INVALID_STATE;
 
     uint8_t buffer[3];
     esp_err_t error;
 
     if (pressure)
     {
-        if ((error = bmx280_read(bmx280, BMX280_REG_PRES_MSB, buffer, sizeof buffer)) != ESP_OK)
+        if ((error = bmp280_read(bmp280, BMP280_REG_PRES_MSB, buffer, sizeof buffer)) != ESP_OK)
             return error;
 
         int32_t adc_P = (buffer[0] << 12) | (buffer[1] << 4) | (buffer[2] >> 4);
-        *pressure = BMP280_compensate_P_int64(bmx280, adc_P);
+        *pressure = BMP280_compensate_P_int64(bmp280, adc_P);
     }
 
     return ESP_OK;
--- main/bmp280.h
+++ main/bmp280.h
@@ -1,5 +1,5 @@
 /**
- * BMX280 - BME280 & BMP280 Driver for Esspressif ESP-32.
+ * BMP280 - BME280 & BMP280 Driver for Esspressif ESP-32.
  *
  * MIT License
  *
@@ -7,8 +7,8 @@
  * Please contact at <utkumaden@hotmail.com>
  */
 
-#ifndef _BMX280_H_
-#define _BMX280_H_
+#ifndef _BMP280_H_
+#define _BMP280_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,50 +19,50 @@ extern "C" {
 #include "driver/i2c.h"
 #include "sdkconfig.h"
 
-#define BMXAPI extern
+#define BMPAPI extern
 
 /**
  * Anonymous structure to driver settings.
  */
-typedef struct bmx280_t bmx280_t;
+typedef struct bmp280_t bmp280_t;
 
 #include "bmp280_bits.h"
 
 /**
- * Create an instance of the BMX280 driver.
+ * Create an instance of the BMP280 driver.
  * @param port The I2C port to use.
  * @return A non-null pointer to the driver structure on success.
  */
-BMXAPI bmx280_t* bmx280_create(i2c_port_t port);
+BMPAPI bmp280_t* bmp280_create(i2c_port_t port);
 
 /**
  * Probe for the sensor and read calibration data.
- * @param bmx280 Driver structure.
+ * @param bmp280 Driver structure.
  */
-BMXAPI esp_err_t bmx280_init(bmx280_t* bmx280);
+BMPAPI esp_err_t bmp280_init(bmp280_t* bmp280);
 
 /**
  * Configure the sensor with the given parameters.
- * @param bmx280 Driver structure.
+ * @param bmp280 Driver structure.
  * @param configuration The configuration to use.
  */
-BMXAPI esp_err_t bmx280_configure(bmx280_t* bmx280, bmx280_config_t *cfg);
+BMPAPI esp_err_t bmp280_configure(bmp280_t* bmp280, bmp280_config_t *cfg);
 
 /**
  * Set the sensor mode of operation.
- * @param bmx280 Driver structure.
+ * @param bmp280 Driver structure.
  * @param mode The mode to set the sensor to.
  */
-BMXAPI esp_err_t bmx280_setMode(bmx280_t* bmx280, bmx280_mode_t mode);
+BMPAPI esp_err_t bmp280_setMode(bmp280_t* bmp280, bmp280_mode_t mode);
 
 /**
  * Read sensor values as fixed point numbers.
- * @param bmx280 Driver structure.
+ * @param bmp280 Driver structure.
  * @param temperature The temperature in C (0.01 degree C increments)
  * @param pressure The pressure in Pa (1/256 Pa increments)
  * @param humidity The humidity in %RH (1/1024 %RH increments) (UINT32_MAX when invlaid.)
  */
-esp_err_t bmx280_readout(bmx280_t *bmx280, uint32_t *pressure);
+esp_err_t bmp280_readout(bmp280_t *bmp280, uint32_t *pressure);
 
 #ifdef __cplusplus
 };
