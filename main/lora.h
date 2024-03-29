e0b1cffeb0855b02fe4490e9f210c42adb41827f
joevento <joe.te.vento@gmail.com>
2024-03-29 17:45:20 +0200

    LORA_SEND_ERROR macro

--- main/lora.h
+++ main/lora.h
@@ -7,6 +7,13 @@
 
 #include <stdlib.h>
 
+#define LORA_SEND_ERROR(tag, x) do {          \
+    esp_err_t err_rc_ = (x);                  \
+    char combined[128];                       \
+    snprintf(combined, sizeof(combined), "%s: %s", (tag), esp_err_to_name(err_rc_)); \
+    lora_transmit(&combined, sizeof(combined)); \
+} while(0)
+
 /*
  * Contains info about the lora module
  */
