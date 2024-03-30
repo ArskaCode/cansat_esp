5b0da5378e124cec82cfe9412c8901bc342d4bc2
joevento <joe.te.vento@gmail.com>
2024-03-30 19:59:30 +0200

    Made the lora_send_log macro

--- main/lora.h
+++ main/lora.h
@@ -7,12 +7,22 @@
 
 #include <stdlib.h>
 
-#define LORA_SEND_ERROR(tag, x) do {          \
-    esp_err_t err_rc_ = (x);                  \
-    char combined[128];                       \
+#define LORA_SEND_ERROR(tag, x) do {            \
+    esp_err_t err_rc_ = (x);                    \
+    char combined[128];                         \
     snprintf(combined, sizeof(combined), "%s: %s", (tag), esp_err_to_name(err_rc_)); \
-    lora_transmit(&combined, sizeof(combined)); \
+    lora_transmit(&combined, strlen(combined)); \
+    if (err_rc_ == ESP_OK){                     \
+        true;                                   \
+    }                                           \
+    false;                                      \
 } while(0)
+
+#define LORA_SEND_LOG(tag, text) do {           \
+    char combined[128];                         \
+    sniprintf(combined, sizeof(combined), "0%s: %s", (tag), (text)); \
+    lora_transmit(&combined, strlen(combined)); \
+} while(0)                                      \
 
 /*
  * Contains info about the lora module
