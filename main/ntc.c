efa91b84753a08d8b7406655bee5e7529ba5c866
joevento <joe.te.vento@gmail.com>
2024-03-31 15:49:55 +0300

    fixed compile error about the ntc thing being static

--- main/ntc.c
+++ main/ntc.c
@@ -63,7 +63,7 @@ void ntc_init(){
 /*---------------------------------------------------------------
         ADC Calibration
 ---------------------------------------------------------------*/
-static bool NTC_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
+bool NTC_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
 {
     adc_cali_handle_t handle = NULL;
     esp_err_t ret = ESP_FAIL;
